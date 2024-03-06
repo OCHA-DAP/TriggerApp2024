bd8ec25195e1d0d575c34e6365c9ade9a6023d09
zackarno <zackarno@gmail.com>
2024-03-06 12:32:28 -0500

    create admin-area lookup data.frame for combining strata forecasts with weighted.means

---
+++ data-raw/admin_area_lookup.R
@@ -0,0 +1,62 @@
+#' Code to create a long data.frame (parquet) that holds each admin unit, it's level (adm1,2,3) and the area of the polygon.
+#' **WHY:** To combine admins for combined historical analysis and thresholding we need areas so we can use the areas as weights for `weighed.mean` calculations
+#' **NOTE:**
+
+
+library(sf)
+library(tidyverse)
+library(janitor)
+library(arrow)
+
+
+CODAB_FP = file.path(
+  Sys.getenv("AA_DATA_DIR"),
+  "public","raw","eth","cod_ab",
+  "eth_adm_csa_bofedb_2021_shp"
+)
+
+admin_layer_names <- paste0("eth_admbnda_adm",1:3,"_csa_bofedb_2021")
+
+# loop through layers clean up names, calc area of each row geom.
+# likely someone will want a map viz added at some point to dashboard therefore..
+# `lgdf` object can be used to generate it.
+lgdf <- set_names(admin_layer_names,admin_layer_names) |>
+  map(
+    \(lyr_nm_tmp){
+      st_read(CODAB_FP,
+              layer = "eth_admbnda_adm3_csa_bofedb_2021") %>%
+        clean_names() %>%
+        mutate(
+          area = as.numeric(st_area(.))
+        ) %>%
+        select(
+          matches("^adm\\d_[ep]|^area|pct_area")
+        )
+    }
+  )
+
+# loop through list of sf objects and put the areas in long format and bind together
+df_area_lookup <- lgdf |>
+  imap(
+  \(gdf_tmp,nm_tmp){
+  df_id <- str_extract(
+    nm_tmp,
+    "adm\\d"
+  )
+  gdf_tmp |>
+    st_drop_geometry() |>
+    select(starts_with(df_id),area) |>
+    mutate(
+      admin_level = df_id
+    )
+  }
+  ) |>
+  list_rbind()
+
+# write as parquet to data folder
+df_area_lookup |>
+  write_parquet(
+    file.path("data",
+              "df_eth_admin_area_lookup.parquet")
+  )
+
